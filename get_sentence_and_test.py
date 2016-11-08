440fcbc7e2a25d725065d422e00d0367b375b4ad
dizwe <dizwe2716@gmail.com>
2016-11-09 00:20:07 +0900

    og태그 오류 해결, 확률 카테고리 한글로 바꾸는 korean 함수 추가, 없는 url도 분석되는 오류 수정.

--- get_sentence_and_test.py
+++ get_sentence_and_test.py
@@ -6,7 +6,7 @@ import requests; from bs4 import BeautifulSoup
 class Guess:
     def __init__(self, fname):
         start_time = time.time()
-        print("It would take about 30 sec")
+        print("It would take about 20 sec")
         with open(fname,'rb') as loaddata:
             self.classifier = pickle.load(loaddata)
         print("load_pickle.")
@@ -20,7 +20,12 @@ class Guess:
        
         category,probability = test(self.classifier,sentence)
         print("guessing category DONE")
-        
+        category,probability = korean(category,probability)
+
+        #필요한 category index 검색해서 확률 뽑아오기
+        one_category_index = [cat for prob,cat in probability].index(category)
+        one_probability = probability[one_category_index][0]
+        print(category+"일 확률 "+str(one_probability)+'%')
         return category,probability
     
     def by_url(self, url):
@@ -30,12 +35,21 @@ class Guess:
         #Return : object classifier(nltk naivebayesclassifier object),
        
         if len(url)==0: #url이 없는 경우
+            print("url이 없습니다. 아무것도 return 되지 않습니다.")
             return "",""
         else:
-            print(url)
-            title,content = spider(url)
-            sentence = str(title) + str(content)
+            #print(url)
+            sentence = spider(url)
+            if len(sentence)<5 :
+                print("글이 충분치 않습니다. 아무것도 return 되지 않습니다.")
+                return "",""
             category,probability = test(self.classifier,sentence)
+            print("guessing category DONE")
+            category,probability = korean(category,probability)
+        
+        one_category_index = [cat for prob,cat in probability].index(category)
+        one_probability = probability[one_category_index][0]
+        print(category+"일 확률 "+str(one_probability)+'%')
 
         return category,probability
 
@@ -51,6 +65,7 @@ def spider(url):
     try:
         source_code = requests.get(url)
     except:#400 error
+        print("없는 페이지 입니다. 아무것도 return 되지 않습니다.")
         return "",""
     
     text = source_code.text
@@ -59,7 +74,6 @@ def spider(url):
     #delete script tag content
     for script in soup.findAll('script'):
         script.extract()
-    og_li = soup.find_all(property = re.compile('og'))
     
     #title
     try:
@@ -68,6 +82,7 @@ def spider(url):
         else:
             title = soup.title.string
     except:#없는 페이지
+        print("없는 페이지 입니다. 아무것도 return 되지 않습니다.")
         title = ""
         
     #content
@@ -80,10 +95,12 @@ def spider(url):
     except:
         content = ""
         
-    return title,content
+    sentence = str(title) + str(content)
+    
+    return sentence
 
 def test(classifier,sentence):
-    #belongs_to : Anywhere, guess_by_url,guess_by_url_list.guess_by_sentence
+    #belongs_to : Anywhere, by_url,guess_by_url_list.guess_by_sentence
     #Role : 카테고리 고르기 main
     #Parameter : object classifier(nltk naivebayesclassifier object), str sentence
     #Return : str category,  list proabability(ex.[(0.95,'sci'),..])
@@ -118,7 +135,7 @@ def determine_category(classifier,dict_of_featuresets,list_of_catageory):
     
     category = classifier.classify(dict_of_featuresets)
     probdist = classifier.prob_classify(dict_of_featuresets)
-    list_of_probability = [(probdist.prob(cat),cat) for cat in list_of_catageory]
+    list_of_probability = [(round(probdist.prob(cat),2)*100,cat) for cat in list_of_catageory]
     
     return category,list_of_probability
 
@@ -131,7 +148,13 @@ def guess_by_url_list(self,classifier,url_list):
     for url in url_list:
         category,probability = guess_by_url(classifier, url)
         category_list.append(category)
-
+    
     print("guessing category DONE")
     return category_list
 
+
+def korean(category,probability) :
+    k_category = {'soc' : '사회','lif':'생활','wor':'세계','pol':'정치','eco':'경제','sit':'IT/과학'}
+    k_probability = [(prob,k_category[cat]) for prob,cat in probability]
+
+    return k_category[category],k_probability
